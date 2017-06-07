2b6fe2a05df382fd6e182854bc1268c7818d2f03
hseitsmstudent <hse.itsm.student@gmail.com>
2017-06-07 21:33:49 +0300

    belyy

---
+++ src/main/java/com/example/demo/Bi3BelyyNew3Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bi3BelyyNew3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bi3BelyyNew3Application.class, args);
+	}
+}
